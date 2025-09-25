8a5a70045c4abfec02f819741fd1d0b6d53a7c94
CriptionX <afifjr321@gmail.com>
2025-09-26 00:24:19 +0700

    fix 26-09-25

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -255,10 +255,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Saiful Amri and Meilla Nadia Fairuzy',
+            dates: `${formatDate('2025-10-17 10:00')}/${formatDate('2025-10-17 19:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'Buaran Gang 1,Kota Pekalongan Selatan',
             ctz: config.get('tz'),
         });
 
